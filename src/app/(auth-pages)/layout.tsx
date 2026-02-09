42408e56ba900a8d5abed996edd5a24d87bab55b
Your Name <your.email@example.com>
2026-02-09 16:48:14 +0300

    first commit

---
+++ src/app/(auth-pages)/layout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react'
+// import Side from '@/components/layouts/AuthLayout/Side'
+import Split from '@/components/layouts/AuthLayout/Split'
+import Simple from '@/components/layouts/AuthLayout/Simple'
+
+const Layout = ({ children }: { children: ReactNode }) => {
+    return (
+        <div className="flex flex-auto flex-col h-[100vh]">
+            {/* <Side> */}
+                {/* <Simple> */}
+<Split>
+                    {children}
+                    </Split>
+                {/* </Simple> */}
+                {/* </Side> */}
+        </div>
+    )
+}
+
+export default Layout
---
+++ src/configs/routes.config/routes.config.ts
@@ -0,0 +1,25 @@
+import authRoute from './authRoute'
+import type { Routes } from '@/@types/routes'
+
+export const protectedRoutes: Routes = {
+    '/home': {
+        key: 'home',
+        authority: [],
+        meta: {
+            pageBackgroundType: 'plain',
+            pageContainerType: 'contained',
+        },
+    },
+    '/page': {
+        key: 'page',
+        authority: [],
+        meta: {
+            pageBackgroundType: 'plain',
+            pageContainerType: 'contained',
+        },
+    },
+}
+
+export const publicRoutes: Routes = {}
+
+export const authRoutes = authRoute
